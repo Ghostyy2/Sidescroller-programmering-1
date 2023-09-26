3888cc63b57f65a9b20169e6ba87e8bf0c9fd0cd
David Ung <david.ung@learnet.se>
2023-09-26 10:55:16 +0200

    New shit ong ong davido is gay

---
+++ Assets/write your name/Moveyourbodyyeahyeahh.cs
@@ -0,0 +1,52 @@
+﻿using UnityEditor.VersionControl;
+using UnityEngine;
+
+public class Moveyourbodyyeahyeahh : MonoBehaviour
+
+{
+    public float Gravitypersecound = 160.0f;
+    public float MovementSpeedpersecond = 10.0f;
+    public float grundlevel = 0.0f;
+  void Update()
+    {
+        Vector3 Gravityposition = transform.position;
+        Gravityposition.y -= Gravitypersecound * Time.deltaTime;
+        if (Gravityposition.y < grundlevel) { Gravityposition.y = grundlevel; }
+        transform.position = Gravityposition;
+
+        //up
+        if (Input.GetKey(KeyCode.W))
+        {
+            Vector3 characterposition = transform.position;
+            characterposition.y += MovementSpeedpersecond * Time.deltaTime;
+            transform.position = characterposition;
+        }
+
+        //left
+        if (Input.GetKey(KeyCode.A))
+        {
+            Vector3 characterposition = transform.position;
+            characterposition.x -= MovementSpeedpersecond * Time.deltaTime;
+            transform.position = characterposition;
+        }
+
+        //down
+        if (Input.GetKey(KeyCode.S))
+        {
+            Vector3 characterposition = transform.position;
+            characterposition.y -= MovementSpeedpersecond * Time.deltaTime;
+            transform.position = characterposition;
+        }
+
+        //right
+        if (Input.GetKey(KeyCode.D))
+        {
+            Vector3 characterposition = transform.position;
+            characterposition.x += MovementSpeedpersecond * Time.deltaTime;
+            transform.position = characterposition;
+        }
+
+    }
+}
+
+
